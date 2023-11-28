5a9ac72571e1b8e11299adf565a866b56db865b2
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2023-11-28 16:14:43 -0500

    DB connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
-import express from "express";
+const express =  require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({path: "config/keys.env"});
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.listen(8080)
+app.use(bodyParser.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+})
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION)
+.then(result=>{
+    app.listen(process.env.PORT)
+})
+.catch(err=> console.log(err))
+
 
