18b960810b62ba2ae5ba33c2859de1b655e4bf33
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2023-12-01 10:01:43 -0500

    Implemented login with JWT token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,9 @@
 const User = require('../models/user');
 const validator = require('../util/validationError')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: '../config/keys.env'})
+
 exports.signup = async(req, res, next)=>{
     validator.validate(req);
     try {
@@ -25,7 +28,34 @@ exports.signup = async(req, res, next)=>{
 
 }
 
-exports.login = (req, res, next)=>{
+exports.login = async (req, res, next)=>{
+    try {
+        //validating email
+        const user = await User.findOne({email: req.body.email.trim()});
+        if(!user){
+            const error = new Error('Wrong email or password!');
+            error.statusCode = 401;
+            throw error;
+        }
+        //validating password
+        const isCorrectPassword = await bcrypt.compare(req.body.password.trim(), user.password);
+        if(!isCorrectPassword){
+            const error = new Error('Wrong email or password!');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({email: user.email, 
+                                userId: user._id.toString()},
+                                process.env.SECRET,
+                                {expiresIn: '1h'});
+        res.status(200).json({
+            token,
+            userId: user._id.toString()
+        })
+    } catch (error) {
+        next(error);
+    }
 
 }
 
