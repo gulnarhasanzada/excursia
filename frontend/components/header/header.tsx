d94b6e9891dee569cbb8db6e307fbaf016ba509f
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2023-12-12 20:43:23 -0500

    Added ProfileMenu component

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import "./header.css"
+import ProfileMenu from "../profile-menu/profile-menu";
+
 const Header = ()=>{
     return (
-        <header className="d-flex ">
-            <div>logo</div>
-            <div>search</div>
-            <div>user</div>
+        <header className="header-container">
+            <div className="header-main  d-flex">
+                <div className="header-item header-logo">
+                    <Link href="/"><img src="/excursia-logo.png" alt="excursia-logo" title="Excursia"/></Link>
+                </div>
+                <div className="header-item">
+
+                </div>
+                <div className="header-item header-profile">
+                    <ProfileMenu />
+                </div>
+            </div>
         </header>
       )
 }
--- frontend/components/layout/layout.tsx
+++ frontend/components/layout/layout.tsx
@@ -1,15 +1,18 @@
 import Footer from "../footer/footer";
 import Header from "../header/header";
+import "./Layout.css"
 
 const Layout = (props: { children: any })=>{
-    return (
+    return (<>
+        <div id="backdrop-root"></div>
+        <div id="overlay-root"></div>
         <div className="general-container">
             <Header/>
             <main className="main-container">
                 {props.children}
             </main> 
             <Footer/>
-        </div>
+        </div></>
       )
 }
 
