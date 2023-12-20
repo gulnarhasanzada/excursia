34d6d7df77b0f8b6d01ada483cd21cd37605a3b7
gulnarhasanzada <48564558+gulnarhasanzada@users.noreply.github.com>
2023-12-19 19:29:49 -0500

    Delete frontend directory

--- frontend/components/footer/footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = ()=>{
-    return(<footer className="footer bg-light text-center p-3">
-               <span className="text-muted">Footer Content</span>
-            </footer>
-    )
-}
-
-export default Footer;
--- frontend/components/util/card/card.tsx
+++
@@ -1,9 +0,0 @@
-import "./card.css"
-
-const Card = (props: any)=>{
-    return <div className="card">
-        {props.children}
-    </div>
-}
-
-export default Card;
